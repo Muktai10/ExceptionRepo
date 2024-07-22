05ea96263a776b21ad5430d4d3f0606b412872c1
Mukta10 <muktapadamwar@gmail.com>
2024-07-22 12:19:09 +0530

    push

---
+++ CustomeException.java
@@ -0,0 +1,31 @@
+package exception1;
+
+
+	class InvalidAgeException extends Exception {
+	    public InvalidAgeException(String message) {
+	        super(message);
+	    }
+	}
+
+	public class CustomeException {
+	    public static void checkAge(int age) throws InvalidAgeException {
+	        if (age < 18) {
+	            throw new InvalidAgeException("Age must be at least 18.");
+	        }
+	        System.out.println("Age is valid.");
+	    }
+
+	    public static void main(String[] args) {
+	        try {
+	            checkAge(16);
+	        } catch (InvalidAgeException e) {
+	            System.out.println("InvalidAgeException caught: " + e.getMessage());
+	        }
+	    }
+	}
+
+	
+
+
+
+
---
+++ MultiCatchBlock.java
@@ -0,0 +1,26 @@
+package exception1;
+
+
+	import java.io.IOException;
+
+	public class MultiCatchBlock {
+	    public static void processFile(String filePath) throws IOException, IllegalArgumentException {
+	        if (filePath == null) {
+	            throw new IllegalArgumentException("File path cannot be null");
+	        }
+	        if (!filePath.endsWith(".txt")) {
+	            throw new IOException("Unsupported file type");
+	        }
+	        
+	    }
+
+	    public static void main(String[] args) {
+	        try {
+	            processFile(null);
+	        } catch (IOException | IllegalArgumentException e) {
+	            System.out.println("Exception caught: " + e.getMessage());
+	        }
+	    }
+	}
+
+
